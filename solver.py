427ced36e4ffc90f2bc46dd0fb8d67bd3b3c930a
rodrikv <107168572+rodrikv@users.noreply.github.com>
2025-01-08 09:45:28 +0000

    improve performance

--- solver.py
+++ solver.py
@@ -1,77 +1,90 @@
 from pprint import pprint
 import time
 
+
 class Sudoku:
     def __init__(self, matrix):
         self.matrix = matrix
+        self.size = 9
+        self.block_size = 3
+        self.rows = [set(range(1, 10)) for _ in range(self.size)]
+        self.cols = [set(range(1, 10)) for _ in range(self.size)]
+        self.blocks = [set(range(1, 10)) for _ in range(self.size)]
+        self._initialize_sets()
 
-    def copy(self):
-        return Sudoku(self.matrix)
+    def _initialize_sets(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                value = self.matrix[i][j]
+                if value:
+                    if value in self.rows[i] and value in self.cols[j] and value in self.blocks[self._block_index(i, j)]:
+                        self.rows[i].discard(value)
+                        self.cols[j].discard(value)
+                        self.blocks[self._block_index(i, j)].discard(value)
+                    else:
+                        raise ValueError(f"Invalid Sudoku: Duplicate value {value} at position ({i}, {j})")
 
-    def set(self, i, j, value):
+    def _block_index(self, i, j):
+        return (i // self.block_size) * self.block_size + (j // self.block_size)
+
+    def set_value(self, i, j, value):
         self.matrix[i][j] = value
+        self.rows[i].discard(value)
+        self.cols[j].discard(value)
+        self.blocks[self._block_index(i, j)].discard(value)
+
+    def unset_value(self, i, j, value):
+        self.matrix[i][j] = 0
+        self.rows[i].add(value)
+        self.cols[j].add(value)
+        self.blocks[self._block_index(i, j)].add(value)
+
+    def get_candidates(self, i, j):
+        return (
+            self.rows[i]
+            & self.cols[j]
+            & self.blocks[self._block_index(i, j)]
+        )
 
 
 def find_next_zero(sudoku: Sudoku):
-    for i in range(len(sudoku.matrix)):
-        for j in range(len(sudoku.matrix[i])):
-            if not sudoku.matrix[i][j]:
+    for i in range(sudoku.size):
+        for j in range(sudoku.size):
+            if sudoku.matrix[i][j] == 0:
                 return i, j
+    return None
 
 
 def solve(sudoku: Sudoku):
-    s = sudoku.copy()
-    x = find_next_zero(s)
-    if not x:
-        pprint(s.matrix)
-        raise Exception()
+    next_pos = find_next_zero(sudoku)
+    if not next_pos:
+        pprint(sudoku.matrix)
+        return True
 
-    i = x[0]
-    j = x[1]
+    i, j = next_pos
+    for candidate in sudoku.get_candidates(i, j):
+        sudoku.set_value(i, j, candidate)
+        if solve(sudoku):
+            return True
+        sudoku.unset_value(i, j, candidate)
 
-    for k in range(1, 10):
-        s.matrix[i][j] = k
-        if check_validity(s, i, j):
-            solve(s)
-    s.matrix[i][j] = 0
-
-
-def check_validity(sudoku: Sudoku, i, j):
-
-    if sudoku.matrix[i][j] in sudoku.matrix[i][:j] or sudoku.matrix[i][j] in sudoku.matrix[i][j+1:]:
-        return False
-
-    for i_ in range(9):
-        if sudoku.matrix[i_][j] == sudoku.matrix[i][j] and not (i_ == i):
-            return False
-
-    start_i = i // 3 * 3
-    end_i = start_i + 3
-    start_j = j // 3 * 3
-    end_j = start_j + 3
-
-    for i_ in range(start_i, end_i):
-        for j_ in range(start_j, end_j):
-            if sudoku.matrix[i_][j_] == sudoku.matrix[i][j] and not (
-                i == i_ and j == j_
-            ):
-                return False
-
-    return True
+    return False
 
 
 def get_sudoku(file_name):
     matrix = []
     with open(file_name) as f:
-        for line in f.readlines():
-            matrix.append(list(map(lambda x: int(x), line.split())))
+        for line in f:
+            matrix.append(list(map(int, line.split())))
     return Sudoku(matrix)
 
 
 if __name__ == "__main__":
-    sudoku = get_sudoku("template.txt")
-    start = time.time()
     try:
-        solve(sudoku)
-    except:
-        print(time.time() - start)
+        sudoku = get_sudoku("template.txt")
+        start = time.time()
+        if not solve(sudoku):
+            print("No solution exists.")
+        print(f"Time taken: {time.time() - start:.4f} seconds")
+    except ValueError as e:
+        print(e)
